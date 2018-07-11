84d03e7aef9acdddaf6170c30a99057fd14ab53f
M4teCocido <rodrigohernandez6684@gmail.com>
2018-07-10 23:32:26 -0300

    tp3

--- TP3/ViajarHoyPagarMañana/js/js.js
+++ TP3/ViajarHoyPagarMañana/js/js.js
@@ -13,6 +13,16 @@ $(document).ready(function(){
 
 	$('#login').click(function(){
 		var id = $('#user').val();
+		if(id == 1){
+			document.location.href = "registrado.html";
+		}
+		if(id == 2){
+			document.location.href = "operador.html";
+		}
+		if(id == 3){
+			document.location.href = "administrador.html";
+		}
+
 		//alert("ingreso de usuario " + id);
 		//document.location.href = newUrl;
 	});
